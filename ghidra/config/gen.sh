ee9f9a3e5c6dc1d09290787d588df858ea894ccc
lol <test@lionsden.home>
2023-03-29 00:44:17 -0400

    clean ghidra gen up

--- ghidra/config/gen.sh
+++ ghidra/config/gen.sh
@@ -13,6 +13,9 @@ cat > config.json <<EOF
 }
 EOF
 
+# Arguments 
+# 1. ghidra data directory
+
 cat >ser.diff<<EOF
 --- /server/server.conf	2020-02-12 12:10:44.000000000 +0000
 +++ server/server_new.conf	2020-11-01 16:47:28.661548715 +0000
@@ -21,7 +24,7 @@ cat >ser.diff<<EOF
  # This variable is also used by the svrAdmin script.
  
 -ghidra.repositories.dir=./repositories
-+ghidra.repositories.dir=/home/ghidra/repos
++ghidra.repositories.dir=$1
  
  # Ghidra server startup parameters.  
  #
@@ -29,8 +32,8 @@ cat >ser.diff<<EOF
  #                       used to store repositories.  Use of this variable to define the
  #                       repositories directory must be retained.
  wrapper.app.parameter.1=-a0
--wrapper.app.parameter.2=${ghidra.repositories.dir}
-+wrapper.app.parameter.5=${ghidra.repositories.dir}
+-wrapper.app.parameter.2=\${ghidra.repositories.dir}
++wrapper.app.parameter.5=\${ghidra.repositories.dir}
  
  # Establish server process owner
  # This should only be used when installing as a service using a nologin
@@ -38,7 +41,7 @@ cat >ser.diff<<EOF
  # Restart failed service after 1 minute delay
  wrapper.ntservice.failure_actions.actions=RESTART
  wrapper.ntservice.failure_actions.actions_delay=60000
-+wrapper.app.account=ghidra
++wrapper.app.account=$USER
 +wrapper.app.parameter.2=-u
 +wrapper.app.parameter.4=-i$WG_IP
 +wrapper.app.parameter.3=-ssh
