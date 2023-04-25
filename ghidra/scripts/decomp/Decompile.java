b11782f4f9a694c43b48e05d73a4574d7d1461f8
lol <test@lionsden.home>
2023-04-25 10:49:39 -0400

    Not sure why ingredient isn't used anymore, nor what it was used for before

--- ghidra/scripts/decomp/Decompile.java
+++ ghidra/scripts/decomp/Decompile.java
@@ -13,55 +13,56 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-//Decompile an entire program
-
+// Decompile an entire program
+// cm: Updating a bit
+// cm: running against 10.2.3
+// 		ingredients are no longer used
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
 
-import ghidra.app.plugin.core.script.Ingredient;
-import ghidra.app.plugin.core.script.IngredientDescription;
+// import ghidra.app.plugin.core.script.Ingredient;
+// import ghidra.app.plugin.core.script.IngredientDescription;
 import ghidra.app.script.GatherParamPanel;
 import ghidra.app.script.GhidraScript;
 import ghidra.app.util.Option;
 import ghidra.app.util.exporter.CppExporter;
 
-public class Decompile extends GhidraScript implements Ingredient {
+// public class Decompile extends GhidraScript implements Ingredient {
+public class Decompile extends GhidraScript {
 
 	@Override
 	public void run() throws Exception {
-		IngredientDescription[] ingredients = getIngredientDescriptions();
-		for (IngredientDescription ingredient : ingredients) {
-			state.addParameter(ingredient.getID(), ingredient.getLabel(), ingredient.getType(),
-				ingredient.getDefaultValue());
-		}
-		// if (!state.displayParameterGatherer("Script Options")) {
-		// 	return;
+		// IngredientDescription[] ingredients = getIngredientDescriptions();
+		// for (IngredientDescription ingredient : ingredients) {
+		// 	state.addParameter(ingredient.getID(), ingredient.getLabel(), ingredient.getType(),
+		// 		ingredient.getDefaultValue());
 		// }
 
-		String[] args = getScriptArgs();
+		// Removing args and getting the output file name from program
+		// String[] args = getScriptArgs();
+		// System.out.println("ARGS: " + Arrays.toString(args));
+		// File outputFile = new File(args[0]);
 
-
-		System.out.println("ARGS: " + Arrays.toString(args));
-
-		// File outputFile = (File) state.getEnvironmentVar("COutputFile");
-		File outputFile = new File(args[0]);
-
+		// Output file name
+		// Convert the unique program id which is a long to a string
+		File outputFile = new File(currentProgram.getName().concat(""+currentProgram.getUniqueProgramID()).concat(".c"));
 
 		CppExporter cppExporter = new CppExporter();
 		List<Option> options = new ArrayList<Option>();
 		options.add(new Option(CppExporter.CREATE_HEADER_FILE, new Boolean(false)));
 		cppExporter.setOptions(options);
+		// Where do state, currentProgram, and monitor come froms
 		cppExporter.setExporterServiceProvider(state.getTool());
 		cppExporter.export(outputFile, currentProgram, null, monitor);
 	}
 
-	@Override
-	public IngredientDescription[] getIngredientDescriptions() {
-		IngredientDescription[] retVal = new IngredientDescription[] {
-			new IngredientDescription("COutputFile", "Output C File", GatherParamPanel.FILE, "") };
-		return retVal;
-	}
+	// @Override
+	// public IngredientDescription[] getIngredientDescriptions() {
+	// 	IngredientDescription[] retVal = new IngredientDescription[] {
+	// 		new IngredientDescription("COutputFile", "Output C File", GatherParamPanel.FILE, "") };
+	// 	return retVal;
+	// }
 
 }
